c902d4ec81c32d7d2bd8cfa83242b84005f09f55
Muhammed Ahmed <ma3574@gmail.com>
2013-10-04 20:00:50 +0100

    FINISHED

--- js/locate.js
+++ js/locate.js
@@ -19,15 +19,10 @@ function getLocation(){
 
 function getJSON() {
 
-    // var requestURI = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" + phonePos.coords.latitude + "+" + phonePos.coords.longitude +"&format=json&num_of_days=1&key=sk635zud4bwuzmnzvhd4d4sg";
-    //var requestURI = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=37.78937" +  "+-122.38912&format=json&num_of_days=1&key=sk635zud4bwuzmnzvhd4d4sg";
-    // var requestURI = "http://ip.jsontest.com/";
-    //var requestURI = "/local.json";
-    //var requestURI = "http://date.jsontest.com/";
-    //alert(requestURI);
-    var inputJSON = { "data": { "current_condition": [ {"cloudcover": "0", "humidity": "26", "observation_time": "04:32 PM", "precipMM": "0.0", "pressure": "1017", "temp_C": "18", "temp_F": "64", "visibility": "16", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "N", "winddirDegree": "350", "windspeedKmph": "28", "windspeedMiles": "17" } ],  "request": [ {"query": "Lat 37.79 and Lon -122.39", "type": "LatLon" } ],  "weather": [ {"date": "2013-10-04", "precipMM": "0.0", "tempMaxC": "26", "tempMaxF": "79", "tempMinC": "13", "tempMinF": "55", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "NE", "winddirDegree": "38", "winddirection": "NE", "windspeedKmph": "26", "windspeedMiles": "16" } ] }};
-    //alert(inputJSON.data.current_condition[0].weatherDesc[0].value);
-    //alert(inputJSON.data.current_condition[0].temp_C);
+    
+    //var inputJSON = { "data": { "current_condition": [ {"cloudcover": "0", "humidity": "26", "observation_time": "04:32 PM", "precipMM": "0.0", "pressure": "1017", "temp_C": "18", "temp_F": "64", "visibility": "16", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "N", "winddirDegree": "350", "windspeedKmph": "28", "windspeedMiles": "17" } ],  "request": [ {"query": "Lat 37.79 and Lon -122.39", "type": "LatLon" } ],  "weather": [ {"date": "2013-10-04", "precipMM": "0.0", "tempMaxC": "26", "tempMaxF": "79", "tempMinC": "13", "tempMinF": "55", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "NE", "winddirDegree": "38", "winddirection": "NE", "windspeedKmph": "26", "windspeedMiles": "16" } ] }};
+    var inputJSON = { "data": { "current_condition": [ {"cloudcover": "0", "humidity": "26", "observation_time": "04:32 PM", "precipMM": "0.0", "pressure": "1017", "temp_C": "3", "temp_F": "64", "visibility": "16", "weatherCode": "113",  "weatherDesc": [ {"value": "Rainy" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "N", "winddirDegree": "350", "windspeedKmph": "28", "windspeedMiles": "17" } ],  "request": [ {"query": "Lat 37.79 and Lon -122.39", "type": "LatLon" } ],  "weather": [ {"date": "2013-10-04", "precipMM": "0.0", "tempMaxC": "26", "tempMaxF": "79", "tempMinC": "13", "tempMinF": "55", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "NE", "winddirDegree": "38", "winddirection": "NE", "windspeedKmph": "26", "windspeedMiles": "16" } ] }};
+    
 
     var weatherArr = new Array();
     weatherArr["Sunny"] = "What a sunny day";
@@ -49,8 +44,15 @@ function getJSON() {
 
     // alert(imgURL);
 
-    document.querySelector('#weather-img').innerHTML = "<img src=\"" + imgURL + "\">";
 
+    if (inputJSON.data.current_condition[0].weatherDesc[0].value == "Sunny") {
+        document.querySelector('#weather-img').innerHTML = "<img src=\"images/sunnyWeather.png\">";
+
+    } else {
+        document.querySelector('#weather-img').innerHTML = "<img src=\"images/rainyWeather.png\">";
+    }
+
+     document.querySelector('#haiku3-display').innerHTML =  "Default line 3 here";
 
 
 
