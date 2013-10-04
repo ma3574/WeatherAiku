3c6223c2535f45d740988011197b4f8c13589db8
Muhammed Ahmed <ma3574@gmail.com>
2013-10-04 19:28:38 +0100

    Got some shizzzz working

--- js/locate.js
+++ js/locate.js
@@ -26,24 +26,33 @@ function getJSON() {
     //var requestURI = "http://date.jsontest.com/";
     //alert(requestURI);
     var inputJSON = { "data": { "current_condition": [ {"cloudcover": "0", "humidity": "26", "observation_time": "04:32 PM", "precipMM": "0.0", "pressure": "1017", "temp_C": "18", "temp_F": "64", "visibility": "16", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "N", "winddirDegree": "350", "windspeedKmph": "28", "windspeedMiles": "17" } ],  "request": [ {"query": "Lat 37.79 and Lon -122.39", "type": "LatLon" } ],  "weather": [ {"date": "2013-10-04", "precipMM": "0.0", "tempMaxC": "26", "tempMaxF": "79", "tempMinC": "13", "tempMinF": "55", "weatherCode": "113",  "weatherDesc": [ {"value": "Sunny" } ],  "weatherIconUrl": [ {"value": "http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0001_sunny.png" } ], "winddir16Point": "NE", "winddirDegree": "38", "winddirection": "NE", "windspeedKmph": "26", "windspeedMiles": "16" } ] }};
-    alert(inputJSON.data.current_condition[0].weatherDesc[0].value);
-    alert(inputJSON.data.current_condition[0].temp_C);
+    //alert(inputJSON.data.current_condition[0].weatherDesc[0].value);
+    //alert(inputJSON.data.current_condition[0].temp_C);
 
     var weatherArr = new Array();
-    weatherArr["Sunny"] = "It's sunny";
+    weatherArr["Sunny"] = "What a sunny day";
+    weatherArr["Rainy"] = "So much rain today";
 
     var haikuText = weatherArr[inputJSON.data.current_condition[0].weatherDesc[0].value];
 
-    document.querySelector('#haiku-display').innerHTML = haikuText;
+    document.querySelector('#haiku1-display').innerHTML = haikuText;
 
-    if (inputJSON.data.current_condition[0].temp_C < 5) {
-        document.querySelector('#haiku-display').appendChild(document.createTextNode("<br/>It's bloody freezing"));
+    if (inputJSON.data.current_condition[0].temp_C < 10) {
+        //document.querySelector('#haiku2-display').appendChild(document.createElement("br"));
+        document.querySelector('#haiku2-display').innerHTML = "It is so bloody freezing!";
     } else {
-        document.querySelector('#haiku-display').appendChild(document.createTextNode("<br/>It's nice and warm"));
+        
+        document.querySelector('#haiku2-display').innerHTML = "It is nice and warm outside!";
     }
 
-    
-    
+    var imgURL = inputJSON.data.current_condition[0].weatherIconUrl[0].value;
+
+    // alert(imgURL);
+
+    document.querySelector('#weather-img').innerHTML = "<img src=\"" + imgURL + "\">";
+
+
+
 
     
 
